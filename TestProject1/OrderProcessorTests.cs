44e08e94bc61c8a8256f6b52c0e1d1ef7fc561b2
Roller Lo <jasonbh02@gmail.com>
2020-12-21 02:42:56 +0800

    unit test for interface testability

--- TestProject1/OrderProcessorTests.cs
+++ TestProject1/OrderProcessorTests.cs
@@ -1,15 +1,42 @@
 ﻿using System;
+using ConsoleApplication1.Testability;
 using NUnit.Framework;
 
 namespace TestProject1
 {
     [TestFixture]
-    public class Tests
+    public class OrderProcessorTests
     {
+        // METHODNAME_CONDITION_EXPECTATION
         [Test]
-        public void Test1()
+        public void Process_OrderIsAlreadyShipped_ThrowAnException()
         {
-            Assert.True(true);
+            var orderProcessor = new OrderProcessor(new FakeShippingCalculator());
+            var order = new Order
+            {
+                Shipment = new Shipment()
+            };
+            Assert.That(() => orderProcessor.Process(order),
+                Throws.TypeOf<InvalidOperationException>());
+        }
+
+        [Test]
+        public void Process_OrderIsNotShipped_ShouldSetTheShipmentPropertyOfTheOrder()
+        {
+            var orderProcessor = new OrderProcessor(new FakeShippingCalculator());
+            var order = new Order();
+            orderProcessor.Process(order);
+            Assert.IsTrue(order.IsShipped);
+            Assert.AreEqual(1, order.Shipment.Cost);
+            Assert.AreEqual(DateTime.Today.AddDays(1), order.Shipment.ShippingDate);
+        }
+    }
+
+    public class FakeShippingCalculator : IShippingCalculator
+    {
+        public float CalculateShipping(Order order)
+        {
+            return 1;
         }
     }
 }
