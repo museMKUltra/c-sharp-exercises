08792a616106d766e7b58473ec508f424d47483d
Roller Lo <jasonbh02@gmail.com>
2020-12-24 15:25:03 +0800

    add little comments

--- ConsoleApplication1/ExceptionHandling/YouTubeApi.cs
+++ ConsoleApplication1/ExceptionHandling/YouTubeApi.cs
@@ -12,10 +12,13 @@ namespace ConsoleApplication1.ExceptionHandling
                 // Access YouTube web service
                 // Read the data
                 // Create a list of Video objects
+                // ...
                 throw new Exception("Oops, some low level YouTube error...");
             }
             catch (Exception e)
             {
+                // Log
+                // ...
                 const string msg = "Could not fetch videos from YouTube.";
                 throw new YouTubeException(msg, e);
                 // msg for specific description, e for problem shooting
